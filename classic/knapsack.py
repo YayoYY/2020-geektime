80e0e00874641c6a98e075f1d75eb5a92e79a47b
YayoYY <346756297@qq.com>
2020-03-25 09:14:34 +0800

    0/1背包问题

--- classic/knapsack.py
+++ classic/knapsack.py
@@ -32,11 +32,12 @@ def knapsack_dp(weights, values, capacity):
     n = len(weights)
     dp = [[0 for j in range(capacity+1)] for i in range(n+1)]
     for i in range(1, n+1):
+        weight = weights[i-1]
         for j in range(1, capacity+1):
-            if weights[i-1] > j:
+            if weight > j:
                 dp[i][j] = dp[i-1][j]
             else:
-                dp[i][j] = max(dp[i-1][j], dp[i-1][j-weights[i-1]]+values[i-1])
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j - weight] + values[i-1])
     return dp[-1][-1]
 
 if __name__ == "__main__":
